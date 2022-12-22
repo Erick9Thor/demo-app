1767e13d16a9ef94a6ae7cddfe41905b4e020618
Erick9Thor <erictoper@gmail.com>
2022-12-22 10:07:19 +0100

    generated customer-portal Angular app

--- apps/customer-portal/src/app/app.module.ts
+++ apps/customer-portal/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-
+import { RouterModule } from '@angular/router';
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' })
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
