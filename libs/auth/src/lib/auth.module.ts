d0e797bbbaaaae0b747dddaf5cba550da198f145
Erick9Thor <erictoper@gmail.com>
2022-12-22 11:24:04 +0100

    Added angular material

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { LoginComponent } from './containers/login/login.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
-import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from '@demo-app/material';
 
 export const authRoutes: Route[] = [
   {
@@ -14,7 +14,7 @@ export const authRoutes: Route[] = [
   { path: 'login', component: LoginComponent },
 ];
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MaterialModule],
   declarations: [LoginComponent, LoginFormComponent],
 })
 export class AuthModule {}
